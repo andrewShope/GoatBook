e22408609bd514cc090de645860bad5c538ad6e1
Andrew Shope <andrew.shope88@gmail.com>
2024-09-05 21:25:39 -0600

    Centered input box on home page. Extended FT for styling to check list view page.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -118,6 +118,19 @@ class NewVisitorTest(LiveServerTestCase):
 		self.browser.set_window_size(1024, 768)
 
 		# She notices the input box is nicely centered
+		inputbox = self.browser.find_element(By.ID, "id_new_item")
+		self.assertAlmostEqual(
+			inputbox.location["x"] + inputbox.size["width"] / 2,
+			512,
+			delta=10,
+		)
+
+		# She starts a new list and sees the input is nicely
+		# centered there too
+		inputbox.send_keys("testing")
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table("1: testing")
+		inputbox = self.browser.find_element(By.ID, "id_new_item")
 		self.assertAlmostEqual(
 			inputbox.location["x"] + inputbox.size["width"] / 2,
 			512,
